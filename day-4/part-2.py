ba9359a7ef3bbab9cbfb9e57d18fd040ecd66c0a
Gabriel Lee <gabriel.lee@credera.co.uk>
2022-12-04 05:21:05 +0000

    day 4 (forgot to convert the strings to ints lol)

---
+++ day-4/part-2.py
@@ -0,0 +1,13 @@
+import re
+import math
+
+input = open("input.txt").read()
+parsed_input = re.findall('(\d+)-(\d+),(\d+)-(\d+)', input)
+
+count = 0
+for pair in parsed_input:
+    a_start, a_end = [int(pair[0]), int(pair[1])]
+    b_start, b_end = [int(pair[2]), int(pair[3])]
+    if (a_start <= b_start and a_end >= b_end) or (a_start >= b_start and a_end <= b_end) or (a_start >= b_start and a_start <= b_end) or (a_end >= b_start and a_end <= b_end):
+        count += 1
+print(count)
